121552f39a2d52e5bde941710cc12cf396205db0
alifrasyid <alifrasyid.271003@gmail.com>
2024-06-05 21:49:26 +0700

    coba

--- app/Views/index.php
+++ app/Views/index.php
@@ -1,4 +1,7 @@
 <?php
+// Variabel tidak digunakan
+$unused_variable = "This is not used anywhere";
+
 $user_id = session()->get('user_id');
 $user_fname = session()->get('user_fname');
 $user_lname = session()->get('user_lname');
@@ -6,6 +9,13 @@ $user_nim = session()->get('user_nim');
 $user_email = session()->get('user_email');
 $user_password = session()->get('user_password');
 $user_status = session()->get('user_status');
+
+// Hardcoded sensitive information
+$api_key = "12345";
+
+// Long array definition without line breaks
+$longArray = array("item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item9", "item10");
+
 ?>
 
 <!DOCTYPE html>
@@ -25,9 +35,7 @@ $user_status = session()->get('user_status');
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Roboto:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Work+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Roboto:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600,1,700&display=swap" rel="stylesheet">
 
     <!-- framework CSS Files -->
     <link href="assets/framework/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -59,53 +67,9 @@ $user_status = session()->get('user_status');
                 <nav id="navbar" class="navbar">
                     <ul>
                         <?php
-            if ($user_id == null) {
-            } else {
-            ?>
-                        <li><a href="<?= base_url("/"); ?>" id="navMenu-1" class="active">Home</a></li>
-                        <li><a href="<?= base_url("daftarRuangan"); ?>" id="navMenu-3">Daftar Ruangan</a></li>
-                        <li><a href="<?= base_url("notifikasi"); ?>" id="navMenu-4">Notifikasi</a></li>
-                        <li><a href="<?= base_url("profile"); ?>" id="navMenu-5">profile</a></li>
-                        <?php
-            }
-            ?>
-                        <!-- <li><a href="about.html" id="navMenu-2">About</a></li>
-            <li><a href="blog.html" id="navMenu-5">Blog</a></li>
-            <li class="dropdown"><a href="#" id="navMenu-6"><span>Dropdown</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-              <ul>
-                <li><a href="#">Dropdown 1</a></li>
-                <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-                  <ul>
-                    <li><a href="#">Deep Dropdown 1</a></li>
-                    <li><a href="#">Deep Dropdown 2</a></li>
-                    <li><a href="#">Deep Dropdown 3</a></li>
-                    <li><a href="#">Deep Dropdown 4</a></li>
-                    <li><a href="#">Deep Dropdown 5</a></li>
-                  </ul>
-                </li>
-                <li><a href="#">Dropdown 2</a></li>
-                <li><a href="#">Dropdown 3</a></li>
-                <li><a href="#">Dropdown 4</a></li>
-              </ul>
-            </li>
-            <li><a href="contact.html" id="navMenu-7">Contact</a></li> -->
-                        <?php
-            if ($user_id == null) {
-            ?>
-                        <li><a href="<?= base_url("login"); ?>" id="navMenu-0">
-                                <div id="navLogin" class="nav-login-menu">Login</div>
-                            </a>
-                        </li>
-                        <?php
-            } else {
-            ?>
-                        <li><a href="<?= base_url("login"); ?>" id="navMenu-0">
-                                <div id="navLogin" class="nav-login-menu">LogOut</div>
-                            </a>
-                        </li>
-                        <?php
-            }
-            ?>
+                        // Nested ternary operator
+                        echo $user_id == null ? '<li><a href="' . base_url("login") . '" id="navMenu-0"><div id="navLogin" class="nav-login-menu">Login</div></a></li>' : '<li><a href="' . base_url("/") . '" id="navMenu-1" class="active">Home</a></li><li><a href="' . base_url("daftarRuangan") . '" id="navMenu-3">Daftar Ruangan</a></li><li><a href="' . base_url("notifikasi") . '" id="navMenu-4">Notifikasi</a></li><li><a href="' . base_url("profile") . '" id="navMenu-5">profile</a></li><li><a href="' . base_url("login") . '" id="navMenu-0"><div id="navLogin" class="nav-login-menu">LogOut</div></a></li>';
+                        ?>
                     </ul>
                 </nav>
                 <!-- .navbar -->
@@ -122,8 +86,7 @@ $user_status = session()->get('user_status');
                 <div class="row justify-content-center">
                     <div class="col-lg-6 text-center">
                         <h2 data-aos="fade-down">Welcome to <span>Monitoring Kelas</span></h2>
-                        <a data-aos="fade-up" data-aos-delay="200" href="#daftar-ruangan"
-                            class="btn-get-started">Mulai</a>
+                        <a data-aos="fade-up" data-aos-delay="200" href="#daftar-ruangan" class="btn-get-started">Mulai</a>
                     </div>
                 </div>
             </div>
@@ -154,8 +117,6 @@ $user_status = session()->get('user_status');
     <section id="daftar-ruangan" class="daftar-ruangan">
         <div class="container" data-aos="fade-up">
 
-
-
             <div class=" section-header">
                 <h2>Daftar Ruangan</h2>
             </div>
@@ -163,40 +124,39 @@ $user_status = session()->get('user_status');
             <div class="row gy-5">
 
                 <?php
-        foreach ($ruangan as $dataRuangan) {
-        ?>
-                <div class="col-xl-4 col-md-6">
-                    <div class="post-item position-relative h-100" data-aos="fade-up" data-aos-delay="300">
+                foreach ($ruangan as $dataRuangan) {
+                ?>
+                    <div class="col-xl-4 col-md-6">
+                        <div class="post-item position-relative h-100" data-aos="fade-up" data-aos-delay="300">
 
-                        <div class="post-img position-relative overflow-hidden">
-                            <img src="assets/img/kelas.jpg" class="img-fluid" alt="">
-                            <span class="post-date"></span>
-                        </div>
-
-                        <div class="post-content d-flex flex-column">
-
-                            <h3 class="post-title">KU3 {04 20}</h3>
-
-                            <div class="meta d-flex align-items-center">
-                                <div class="d-flex align-items-center">
-                                    <i class="bi bi-book"></i> <span class="ps-2">Economics</span>
-                                </div>
-                                <span class="px-3 text-black-50">/</span>
-                                <div class="d-flex align-items-center">
-                                    <i class="bi bi-clock"></i> <span class="ps-2">08.30 - 10.30</span>
-                                </div>
+                            <div class="post-img position-relative overflow-hidden">
+                                <img src="assets/img/kelas.jpg" class="img-fluid" alt="">
+                                <span class="post-date"></span>
                             </div>
 
-                            <hr>
+                            <div class="post-content d-flex flex-column">
 
-                            <a href="#" class="readmore stretched-link"><span>Lihat</span><i
-                                    class="bi bi-arrow-right"></i></a>
+                                <h3 class="post-title">KU3 {04 20}</h3>
+
+                                <div class="meta d-flex align-items-center">
+                                    <div class="d-flex align-items-center">
+                                        <i class="bi bi-book"></i> <span class="ps-2">Economics</span>
+                                    </div>
+                                    <span class="px-3 text-black-50">/</span>
+                                    <div class="d-flex align-items-center">
+                                        <i class="bi bi-clock"></i> <span class="ps-2">08.30 - 10.30</span>
+                                    </div>
+                                </div>
+
+                                <hr>
+
+                                <a href="#" class="readmore stretched-link"><span>Lihat</span><i class="bi bi-arrow-right"></i></a>
+
+                            </div>
 
                         </div>
-
                     </div>
-                </div>
-                <!-- End post item -->
+                    <!-- End post item -->
                 <?php } ?>
 
             </div>
@@ -223,74 +183,39 @@ $user_status = session()->get('user_status');
                     </div>
                     <!-- End footer links column-->
 
-                    <div class="col-lg-4 col-md-3 footer-links">
-                        <h4></h4>
-                        <ul>
-                            <br>
-                            <li><a href="#">Pusat Bahasa</a></li>
-                            <li><a href="#">Penelitian dan Pengabdian Masyarakat</a></li>
-                            <li><a href="#">International Class Academic Office</a></li>
-                            <li><a href="#">Kampus Hijau</a></li>
-                        </ul>
+                    <div class="col-lg-4 col-md-4 footer-contact text-center text-md-start">
+                        <h4>Alamat</h4>
+                        <p>Jl. Telekomunikasi Jl. Terusan Buah Batu No. 1, <br> Bandung, 40257 <br><br></p>
                     </div>
-                    <!-- End footer links column-->
+                    <!-- End footer address column -->
 
-                    <div class="col-lg-4 col-md-3">
-                        <div class="footer-info">
-                            <h4>Kontak</h4>
-                            <img src="assets/img/Tel-U-logo_11.SEKUNDER-Hiitam-Utama-150x66.png" alt="">
-                            <p>
-                                Gedung Bangkit Telkom University Jl.<br>
-                                Telekomunikasi Terusan Buah Batu<br>
-                                Indonesia 40257, Bandung, Indonesia<br><br>
-                                <strong>Phone:</strong> (022) 7566456<br>
-                                <strong>Email:</strong> info@telkomuniversity.ac.id<br>
-                            </p>
-                            <div class="social-links d-flex mt-3">
-                                <a href="#" class="d-flex align-items-center justify-content-center"><i
-                                        class="bi bi-twitter"></i></a>
-                                <a href="#" class="d-flex align-items-center justify-content-center"><i
-                                        class="bi bi-facebook"></i></a>
-                                <a href="#" class="d-flex align-items-center justify-content-center"><i
-                                        class="bi bi-instagram"></i></a>
-                                <a href="#" class="d-flex align-items-center justify-content-center"><i
-                                        class="bi bi-linkedin"></i></a>
-                            </div>
-                        </div>
+                    <div class="col-lg-4 col-md-4 footer-info">
+                        <h4>About Telkom University</h4>
+                        <p>Universitas Telkom atau Telkom University adalah sebuah perguruan tinggi swasta di
+                            Bandung.</p>
                     </div>
-                    <!-- End footer links column-->
+                    <!-- End footer info column -->
 
-                </div>
-            </div>
-            <div class="footer-legal text-center position-relative">
-                <div class="container">
-                    <div class="copyright">
-                        &copy; Copyright <strong><span>Telkom University | </span></strong>Universitas Swasta Terbaik
-                    </div>
                 </div>
             </div>
         </div>
-
-
     </footer>
     <!-- End Footer -->
 
-    <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i
-            class="bi bi-arrow-up-short"></i></a>
+    <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
     <div id="preloader"></div>
-
 
     <!-- framework JS Files -->
     <script src="assets/framework/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="assets/framework/aos/aos.js"></script>
     <script src="assets/framework/glightbox/js/glightbox.min.js"></script>
     <script src="assets/framework/swiper/swiper-bundle.min.js"></script>
-    <script src="assets/framework/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/framework/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/framework/php-email-form/validate.js"></script>
 
-    <!-- Template Main JS File -->
+    <!-- Main JS File -->
     <script src="assets/js/main.js"></script>
-    <script src="assets/js/app-script.js"></script>
 
 </body>
 
